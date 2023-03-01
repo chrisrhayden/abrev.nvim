746adfab6950a4da1342a170cf9255ca56339699
chrisrhayden <chrisrhayden@gmail.com>
2023-03-01 13:40:02 -0800

    removing dev items

--- lua/abrev/init.lua
+++ lua/abrev/init.lua
@@ -5,19 +5,6 @@
 ---there is no way to associate correct variants out of
 ---order so this works fine
 
-function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then k = '"' .. k .. '"' end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
-
 local M = {}
 
 --TODO: add comments to pattern
